107bc58655c404a026d9a326cb7135f621b6fd35
Federico Speltini <fedesp71@gmail.com>
2021-01-04 20:34:10 -0300

    test

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { AppImgur } from './AppImgur';
 import './index.css';
-
 //import 'bootstrap/dist/css/bootstrap.min.css';
 import 'jquery';
 import 'popper.js';
